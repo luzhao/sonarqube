e92578d30c12f386e15ec3f6777245cbf4d667bf
Stas Vilchik <vilchiks@gmail.com>
2015-02-26 18:02:06 +0100

    better placement of test files

--- server/sonar-web/src/test/server.js
+++ server/sonar-web/src/test/server.js
@@ -1,11 +1,9 @@
 var express = require('express'),
     path = require('path'),
     errorhandler = require('errorhandler'),
-    serveStatic = require('serve-static'),
-    im = require('istanbul-middleware');
+    serveStatic = require('serve-static');
 
-var staticPath = path.join(__dirname, '../../../webapp');
-im.hookLoader(staticPath);
+var staticPath = path.join(__dirname, '../main/webapp');
 
 var app = express();
 
@@ -14,8 +12,6 @@ app.set('view engine', 'jade');
 
 app.use(errorhandler({ dumpExceptions: true, showStack: true }));
 
-app.use(im.createClientHandler(staticPath));
-app.use('/coverage', im.createHandler());
 app.use('/', serveStatic(staticPath));
 
 app.get('/pages/:page', function (req, res) {
